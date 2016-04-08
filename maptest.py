4a75f22acaf3075070a01b6dfebd43054cdc08a8
Chris Rogers <chris.rogers@bodleian.ox.ac.uk>
2016-04-08 23:56:19 +0100

    arghg

--- maptest.py
+++ maptest.py
@@ -6,34 +6,34 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/runmap', methods=['GET', 'POST'])
 def index():
     return render_template('map_test.html')
 
-# @app.context_processor
-# def inject_data():
-#     folder = os.path.dirname(os.path.realpath(__file__))+'/activities'
-#
-#     gps_points = []
-#
-#     for subdir, dirs, files in os.walk(folder):
-#         filenamer = 1
-#         data = {}
-#         for filename in files:
-#             file_path = (subdir+'/'+filename)
-#             try:
-#                 soup = BeautifulSoup(open(file_path), 'xml')
-#             except(UnicodeDecodeError):
-#                 six.print_(file_path)
-#             trps = soup.find_all('trkpt')
-#             local_points = []
-#             for point in trps:
-#                 lat = float(point['lat'])
-#                 lon = float(point['lon'])
-#                 local_points.append([lat, lon])
-#             data["data"+str(filenamer)] = local_points
-#             filenamer = filenamer + 1
-#     return dict(gpsdata=data)
+@app.context_processor
+def inject_data():
+    folder = os.path.dirname(os.path.realpath(__file__))+'/activities'
+
+    gps_points = []
+
+    for subdir, dirs, files in os.walk(folder):
+        filenamer = 1
+        data = {}
+        for filename in files:
+            file_path = (subdir+'/'+filename)
+            try:
+                soup = BeautifulSoup(open(file_path), 'xml')
+            except(UnicodeDecodeError):
+                six.print_(file_path)
+            trps = soup.find_all('trkpt')
+            local_points = []
+            for point in trps:
+                lat = float(point['lat'])
+                lon = float(point['lon'])
+                local_points.append([lat, lon])
+            data["data"+str(filenamer)] = local_points
+            filenamer = filenamer + 1
+    return dict(gpsdata=data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
