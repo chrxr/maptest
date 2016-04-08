370e46c7f0cc9d29323a68a1070933f7333373c4
Chris Rogers <chris.rogers@bodleian.ox.ac.uk>
2016-04-08 13:51:14 +0100

    added imports:

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,4 @@
 from data_munge import app
-import os
-import json
-import six
-from flask import Flask, render_template, request
-from bs4 import BeautifulSoup
-
 
 if __name__ == "__main__":
     app.run()
