f884cc6884b93f649d996f2c789e7d3f15eede2d
Chris Rogers <chris.rogers@bodleian.ox.ac.uk>
2016-04-12 13:01:19 +0100

    added a redis clear function for adding new files

--- maptest.py
+++ maptest.py
@@ -7,42 +7,49 @@ import redis
 
 app = Flask(__name__)
 
-@app.route('/runmap', methods=['GET', 'POST'])
-def index():
-    return render_template('map_test.html')
-
-@app.context_processor
-def inject_data():
+@app.route('/runmap/')
+@app.route('/runmap/<command>')
+def index(command = None):
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     folder = os.path.dirname(os.path.realpath(__file__))+'/activities'
 
-    gps_points = []
-    if r.get('data'):
-        data = json.loads(r.get('data'))
-        return dict(gpsdata = data)
+    if command == "clear":
+        routes = generate_routes(folder, r)
+        return render_template('map_test.html', gpsdata = routes)
     else:
-        for subdir, dirs, files in os.walk(folder):
-            filenamer = 1
-            data = {}
+        if r.get('data'):
+            routes = json.loads(r.get('data'))
+            return render_template('map_test.html', gpsdata = routes)
+        else:
+            routes = generate_routes(folder, r)
+            return render_template('map_test.html', gpsdata = routes)
 
-            for filename in files:
-                file_path = (subdir+'/'+filename)
-                try:
-                    soup = BeautifulSoup(open(file_path), 'xml')
-                except(UnicodeDecodeError):
-                    six.print_(file_path)
-                trps = soup.find_all('trkpt')
-                local_points = []
-                for point in trps:
-                    lat = float(point['lat'])
-                    lon = float(point['lon'])
-                    local_points.append([lat, lon])
-                data["data"+str(filenamer)] = local_points
-                data_json = json.dumps(data)
-                r.set('data', data_json)
-                print r.get('data')
-                filenamer = filenamer + 1
-        return dict(gpsdata=data)
+def generate_routes(folder, r):
+    for subdir, dirs, files in os.walk(folder):
+        filenamer = 1
+        data = {}
+
+        for filename in files:
+            file_path = (subdir+'/'+filename)
+            try:
+                soup = BeautifulSoup(open(file_path), 'xml')
+            except(UnicodeDecodeError):
+                six.print_(file_path)
+            trps = soup.find_all('trkpt')
+            local_points = []
+            for point in trps:
+                lat = float(point['lat'])
+                lon = float(point['lon'])
+                local_points.append([lat, lon])
+            data["data"+str(filenamer)] = local_points
+            data_json = json.dumps(data)
+            filenamer = filenamer + 1
+        try:
+            r.delete('data')
+        except:
+            pass
+        r.set('data', data_json)
+    return data
 
 if __name__ == "__main__":
     app.debug = True
